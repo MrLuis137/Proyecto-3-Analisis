083f2edd843a487ca8f17fcb73afe0a24f2fbaea
JacobPG <51519160+JacobPG@users.noreply.github.com>
2020-12-02 19:27:00 -0600

    Retoques

--- Proyecto-3-Analisis/src/main/java/principal/Flor.java
+++ Proyecto-3-Analisis/src/main/java/principal/Flor.java
@@ -12,48 +12,60 @@ import java.util.ArrayList;
  */
 public class Flor {
     
-    private float posX;
-    private float posY;
+    private int posX;
+    private int posY;
     private Color colorFav;
+    private Flor ancestros[];
     private ArrayList<Flor> flores = new ArrayList<Flor>();//Flores de las cuales obtuvo polen.
 
     
-    Flor(float posX, float posY, Color colorFav){
+    Flor(){
+        //this.pos =(int) (Math.random() * tamaño de la ventana);
+        this.posX = (int) (Math.random() * 100);
+        this.posY = (int) (Math.random() * 100);
+        this.colorFav = new Color();
+    }
+    
+    Flor(int posX, int posY, Color colorFav, Flor ancestro1, Flor ancestro2){
         this.posX=posX;
         this.posY=posY;
         this.colorFav=colorFav;
+        this.ancestros[0]=ancestro1;
+        this.ancestros[1]=ancestro2;   
     }
     
     public void agregarFlor(Flor flor){
         flores.add(flor);
+        //flores.addAll(flores)//Si lo que va agregar de una un arraylist entero
     }
-
-    public float getPosX() {
+    
+    public int getPosX() {
         return posX;
     }
-
-    public void setPosX(float posX) {
+    public void setPosX(int posX) {
         this.posX = posX;
     }
 
-    public float getPosY() {
-        return posY;
-    }
-
-    public void setPosY(float posY) {
+    public void setPosY(int posY) {
         this.posY = posY;
     }
-
+    public int getPosY() {
+        return posY;
+    }
+    
     public Color getColorFav() {
         return colorFav;
     }
-
     public void setColorFav(Color colorFav) {
         this.colorFav = colorFav;
     }
     
     public ArrayList<Flor> getFlores() {
         return flores;
+    }
+
+    public Flor[] getAncestros() {
+        return ancestros;
     }
     
 }
