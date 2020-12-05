d06890a94b875fae2ef08bdb6343e195bb6e2ff6
JacobPG <51519160+JacobPG@users.noreply.github.com>
2020-12-04 22:24:36 -0600

    Agregado el cruce (sin probar)

--- Proyecto-3-Analisis/src/main/java/logica/Flor.java
+++ Proyecto-3-Analisis/src/main/java/logica/Flor.java
@@ -15,23 +15,25 @@ public class Flor {
     private int posX;
     private int posY;
     private int color;
+    private int generacion;
     private Flor ancestros[];
     private ArrayList<Flor> flores = new ArrayList<Flor>();//Flores de las cuales obtuvo polen.
 
     
-    Flor(){
+    public Flor(){
         //this.pos =(int) (Math.random() * tamaño de la ventana);
         this.posX = (int) (Math.random() * 100);
         this.posY = (int) (Math.random() * 100);
-        this.color = (int) (Math.random() * 7);
+        this.color = (int) (Math.random() * 8);
+        this.generacion=0; 
     }
     
-    Flor(int posX, int posY, int color, Flor ancestro1, Flor ancestro2){
+    public Flor(int posX, int posY, int color, Flor ancestro1, Flor ancestro2){
         this.posX=posX;
         this.posY=posY;
         this.color=color;
-        this.ancestros[0]=ancestro1;
-        this.ancestros[1]=ancestro2;   
+        this.generacion=ancestro1.getGeneracion()+1;
+        this.ancestros = new Flor[]{ancestro1, ancestro2}; 
     }
     
     public void agregarFlor(Flor flor){
@@ -59,7 +61,11 @@ public class Flor {
     public void setColor(int color) {
         this.color = color;
     }
-    
+
+    public int getGeneracion() {
+        return generacion;
+    }
+        
     public ArrayList<Flor> getFlores() {
         return flores;
     }
@@ -68,4 +74,39 @@ public class Flor {
         return ancestros;
     }
     
+    public String completarBits(String numero, int tipo){
+        if(tipo==0)while(numero.length()<3)numero="0"+numero;  
+        else while(numero.length()<8)numero="0"+numero;
+        return numero;    
+    }
+    
+    public String obtenerCadena(Flor flor){
+        String cadena="";
+        //Posicion
+        cadena.concat(completarBits(Integer.toBinaryString(flor.getPosX()),1));
+        cadena.concat(completarBits(Integer.toBinaryString(flor.getPosY()),1));
+        //Color
+        cadena.concat(completarBits(Integer.toBinaryString(flor.getColor()),0));
+        return cadena; 
+    }
+    public Flor generarFlor(String cadena){
+        
+        return null;
+    }
+            
+    public Flor cruzarFlores(){
+        int indice = (int)(Math.random()*(flores.size()));
+        
+        String individuo1=obtenerCadena(this);
+        String individuo2=obtenerCadena(flores.get(indice));
+        
+        int corte=(int)(Math.random()*19);
+        
+        String nuevo= individuo1.substring(0, corte)+individuo2.substring(corte, individuo2.length());
+        
+        //Mutación
+
+        return new Flor(Integer.parseInt(nuevo.substring(0, 8)),Integer.parseInt(nuevo.substring(8, 16)),
+                Integer.parseInt(nuevo.substring(16, 19)),this,flores.get(indice));
+    }
 }
