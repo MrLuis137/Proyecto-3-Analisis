7794d7915fcd800de9eaf39d253a75c4a9b0722b
JacobPG <51519160+JacobPG@users.noreply.github.com>
2020-12-12 14:42:18 -0600

    Búsqueda y Recorrido
    > Se descomentó los atributos posicionInicialXY
    > Se agregó el ángulo de desviación

--- Proyecto-3-Analisis/src/logica/Abeja.java
+++ Proyecto-3-Analisis/src/logica/Abeja.java
@@ -21,8 +21,11 @@ public class Abeja {
     static private int globalgen = 0;
     String id = "";
     //====== IRRELEVANTE PARA LA BUSQUEDA, CRUCE ETC..=============
-    //int puntoInicioX = 50;
-    //int puntoInicioY = 50;
+    int puntoInicioX = 50;
+    int puntoInicioY = 50;
+    
+    private int angulo;//Angulo de desviacion 
+    
     boolean seAleja;
     boolean mutada = false;
     private double factorMutacion = 0.5;
@@ -54,6 +57,7 @@ public class Abeja {
                 f.agregarFlores(floresVisitadas);
                 this.posX = f.getPosX();
                 this.posY = f.getPosY();
+                
                 return 1;
             }
         }
@@ -69,9 +73,15 @@ public class Abeja {
         this.orden = (int)(Math.random() * 3);
         //System.out.println(this.orden);
         this.distMax  = (int) (Math.random() * (100 - 10) + 10);
+        this.angulo= (int) (Math.random() * (25 - 10) + 25);
         this.colorFav = (int) (Math.random() * 7);
         this.direccionFav = Direccion.getDireccion((int) Math.random() * 7);
         this.generacion = 0;
+        
+        if( (int)(Math.random() * 2)==1){seAleja=true;}
+        else{seAleja=false;}
+                
+        
         /*if((int)(Math.random()*1) == 0){
            posX = puntoInicioX;
            posY = puntoInicioY;
@@ -192,7 +202,8 @@ public class Abeja {
     
     public void setMutada(boolean isMutada){mutada = isMutada;}
     
-
+    public int getAngulo() {return angulo;}
+    
     public int getGeneracion() {
         return generacion;
     }
@@ -336,6 +347,284 @@ public class Abeja {
 
     public float getPuntaje() {
         return puntaje;
+    }
+    
+    //_______________________BUSQUEDA_______________________
+    //______________________________________________________
+    public int distanciaInt(int x1, int y1, int x2, int y2){
+        return (int) Math.round(Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2)));
+    }
+    public double distanciaFloat(int x1, int y1, int x2, int y2){
+        return Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+    }
+
+    //__________________________Obtener punto final
+    //Norte
+    public int pointN(int y, int dist){ return y-dist;}
+    //Sur
+    public int pointS(int y, int dist){ return y+dist;}
+    //Este
+    public int pointE(int x, int dist){ return x+dist;}
+    //Oeste
+    public int pointO(int x, int dist){ return x-dist;}
+    
+    //Norteste
+    public int pointNE(int x, int dist){
+        int a=x;
+        while(true){
+            if(distanciaInt(x,x,a+1,x-((a+1)-x))>dist) return a;
+            a+=1;
+        }
+    }
+    //Sureste
+    public int pointSE(int x, int dist){
+        int a=x;
+        while(true){
+            if(distanciaInt(x,x,a+1,a+1)>dist) return a;
+            a+=1;
+        }
+    }       
+    //Noroeste
+    public int pointNO(int x, int dist){
+        int a=x;
+        while(true){
+            if(distanciaInt(x,x,a-1,a-1)>dist) return a;
+            a-=1;
+        }
+    }
+    //Suroeste
+    public int pointSO(int x, int dist){ 
+        int a=x;
+        while(true){
+            if(distanciaInt(x,x,x-((a+1)-x),a+1)>dist) return a;
+            a+=1;
+        }
+    }     
+    //Obtener puntos secundarios
+    public int [] getRotatePoint(int pivoteX, int pivoteY, int finalX, int finalY, int angle){
+        //System.out.println("ENTRO GETPOINT");
+        float newX,newY,x,y;
+        x=finalX-pivoteX;y=finalY-pivoteY;
+        
+        float newAngle = (float) (angle*(Math.PI/180)); 
+        float cos=(float) Math.cos(newAngle);
+        float sen=(float) Math.sin(newAngle);
+        
+        newX=(x*cos)-(y*sen);newY=(y*cos)+(x*sen);
+        newX+=pivoteX; newY+=pivoteY;
+        
+        if(newX<0)newX=0;
+        else if (newX>100)newX=100;
+        
+        if(newY<0)newY=0;
+        else if (newY>100)newY=100;
+         
+        int[] posicion = {Math.round(newX), Math.round(newY)}; 
+        //System.out.println("TERMINÓ GETPPOINT");
+        return posicion;
+    }    
+    
+    private static double calculateAngle(int P1X, int P1Y, int P2X, int P2Y, int P3X, int P3Y){
+        double numerator = P2Y*(P1X-P3X) + P1Y*(P3X-P2X) + P3Y*(P2X-P1X); 
+        double denominator = (P2X-P1X)*(P1X-P3X) + (P2Y-P1Y)*(P1Y-P3Y); 
+        double ratio = numerator/denominator; 
+        double angleRad = Math.atan(ratio); 
+        double angleDeg = (angleRad*180)/Math.PI; 
+        if(angleDeg<0){ 
+            angleDeg = 180+angleDeg;
+        }else if(angleDeg==0.0){
+            angleDeg=180.0;
+        } 
+        return angleDeg; } 
+    
+    public static boolean dentro(int fx,int fy,int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4){
+        double total=0;
+        //Triangulo f,1,2
+        total+=calculateAngle(fx, fy, x1, y1, x2, y2);//System.out.println(calculateAngle(fx, fy, x1, y1, x2, y2));
+        //Triangulo f,2,3
+        total+=calculateAngle(fx, fy, x2, y2, x3, y3);//System.out.println(calculateAngle(fx, fy, x2, y2, x3, y3));
+        //Triangulo f,3,4
+        total+=calculateAngle(fx, fy, x3, y3, x4, y4);//System.out.println(calculateAngle(fx, fy, x3, y3, x4, y4));
+        //Triangulo f,4,1
+        total+=calculateAngle(fx, fy, x4, y4, x1, y1);//System.out.println(calculateAngle(fx, fy, x4, y4, x1, y1));
+        //System.out.println(total);
+        if(Double.isNaN(total)){
+            return true;
+        }
+        return total==360.0;
+    }
+    
+    public boolean posibilidad(){return (int) (Math.random() * 100)<5;}//SI VISITA UNA FLOR DE DIFERENTE COLOR
+    
+    public void recorrido(ArrayList<Integer> posibles){
+        
+        //System.out.println("ENTRO RECORRIDO");//PRINT DE PRUEBA
+        
+        boolean derecha=false;
+        switch (this.getDireccionFav()) {
+            case N: 
+                derecha=true;break;
+            case S:
+                derecha=true;break;
+            case E:
+                derecha=true;break;
+            case NE:
+                derecha=true;break;
+            case SE:
+                derecha=true;break;}
+        int cont;
+        double dist;
+        Flor flower;
+        this.posX=this.puntoInicioX;this.posY=this.puntoInicioY;
+        if (derecha){
+                if (seAleja){
+                    cont=0;
+                    while (cont<posibles.size()){
+                        flower=Main.FloresActuales.get(posibles.get(cont));
+                        dist=(distanciaFloat(this.posX,this.posY,flower.getPosX(),flower.getPosY())+this.distanciaRecorrida);
+                        if (dist>this.getDistMax()){
+                            cont++;continue;
+                        }this.distanciaRecorrida=(float) dist;
+
+                        for(Flor a: this.floresVisitadas){flower.agregarFlor(a);}
+                        this.agregarFlor(flower);
+                        this.posX=flower.getPosX();this.posY=flower.getPosY();
+                        Main.FloresActuales.set(cont, flower);
+                        cont++;}
+                }else{
+                    cont=posibles.size()-1;
+                    while (cont>-1){
+                        flower=Main.FloresActuales.get(posibles.get(cont));
+                        dist=(distanciaFloat(this.posX,this.posY,flower.getPosX(),flower.getPosY())+this.distanciaRecorrida);
+                        if (dist>this.getDistMax()){
+                            cont--;continue;
+                        }this.distanciaRecorrida=(float) dist;
+
+                        for(Flor a: this.floresVisitadas){flower.agregarFlor(a);}
+                        this.agregarFlor(flower);
+                        this.posX=flower.getPosX();this.posY=flower.getPosY();
+                        Main.FloresActuales.set(cont, flower);
+                        cont--;}   
+                }
+            }else{
+                if (seAleja){                    
+                    cont=posibles.size()-1;
+                    while (cont>-1){
+                        flower=Main.FloresActuales.get(posibles.get(cont));
+                        dist=(int) (distanciaFloat(this.posX,this.posY,flower.getPosX(),flower.getPosY())+this.distanciaRecorrida);
+                        if (dist>this.getDistMax()){
+                            cont--;continue;
+                        }this.distanciaRecorrida=(float) dist;
+
+                        for(Flor a: this.floresVisitadas){flower.agregarFlor(a);}
+                        this.agregarFlor(flower);
+                        this.posX=flower.getPosX();this.posY=flower.getPosY();
+                        Main.FloresActuales.set(cont, flower);
+                        cont--;}
+                }else{
+                    cont=0;
+                    while (cont<posibles.size()){
+                        flower=Main.FloresActuales.get(posibles.get(cont));
+                        dist=(distanciaFloat(this.posX,this.posY,flower.getPosX(),flower.getPosY())+this.distanciaRecorrida);
+                        if (dist>this.getDistMax()){
+                            cont++;continue;
+                        }this.distanciaRecorrida=(float) dist;
+                        for(Flor a: this.floresVisitadas){flower.agregarFlor(a);}
+                        this.agregarFlor(flower);
+                        this.posX=flower.getPosX();this.posY=flower.getPosY();
+                        Main.FloresActuales.set(cont, flower);
+                        cont++;}     
+                }
+            }
+        //System.out.println("TERMINÓ RECORRIDO");//PRINT DE PRUEBA
+    }
+    
+    public void busqueda(){
+        //Obtener el punto max
+        //Punto max
+        
+        //System.out.println("ENTRO BUSQUEDA");//PRINT DE PRUEBA
+        
+        int maxX=0,maxY=0;
+        //MasAngle
+        int masX=0, masY=0;  
+        //MenosAngle
+        int menosX=0,menosY=0;
+        
+        switch (this.getDireccionFav()) {
+            case N: 
+                maxX=this.puntoInicioX;maxY=pointN(this.puntoInicioY,this.distMax);break;
+            case S:
+                maxX=this.puntoInicioX;maxY=pointS(this.puntoInicioY,this.distMax);break;
+            case E:
+                maxY=this.puntoInicioY;maxX=pointE(this.puntoInicioX,this.distMax);break;
+            case O:
+                maxY=this.puntoInicioY;maxX=pointO(this.puntoInicioX,this.distMax);break;
+            case NE:
+                maxX=pointNE(this.puntoInicioX,this.distMax);maxY=(this.puntoInicioX-(maxX-this.puntoInicioX));break;
+            case SE:
+                maxX=maxY=pointSE(this.puntoInicioX,this.distMax);break;
+            case NO:
+                maxX=maxY=pointNO(this.puntoInicioX,this.distMax);
+                break;
+            case SO:
+                maxY=pointNE(this.puntoInicioY,this.distMax);maxX=this.puntoInicioY-(maxY-this.puntoInicioY);break;
+        }
+        
+        ArrayList<Integer> posibles = new ArrayList<>();
+        
+        masX=getRotatePoint(this.puntoInicioX, this.puntoInicioY, maxX, maxY, this.getAngulo())[0];
+        masY=getRotatePoint(this.puntoInicioX, this.puntoInicioY, maxX, maxY, this.getAngulo())[1];
+        menosX=getRotatePoint(this.puntoInicioX, this.puntoInicioY, maxX, maxY, -this.getAngulo())[0];
+        menosY=getRotatePoint(this.puntoInicioX, this.puntoInicioY, maxX, maxY, -this.getAngulo())[1];
+        Integer a;
+        
+        if (this.getOrden()!=2){
+            if (this.orden==0){//Anchura
+                for(Flor flower: Main.FloresActuales){
+                    if(dentro(flower.getPosX(),flower.getPosY(),this.puntoInicioX,this.puntoInicioY,
+                    masX,masY,maxX,maxY,menosX,menosY) && (flower.getColor()==this.getColorFav()||posibilidad())){
+                        
+                        /* SABER SI ENTRÓ UNA FLOR QUE NO ES DEL MISMO COLOR
+                        if(flower.getColor()!=this.getColorFav()){System.out.println("NO ES DEL MISMO COLOR");}//*/
+                        
+                        if(posibles.isEmpty()){posibles.add(Main.FloresActuales.indexOf(flower)); continue;}
+                        for (int i = 0; i < posibles.size(); i++) {
+                            a=posibles.get(i);
+                            if(flower.getPosY()<Main.FloresActuales.get(a).getPosY()){
+                                posibles.add(posibles.indexOf(a), Main.FloresActuales.indexOf(flower));
+                            }else posibles.add(posibles.indexOf(a)+1, Main.FloresActuales.indexOf(flower));
+                            break;}  
+                    }}    
+            }else{//Profundidad
+                for(Flor flower: Main.FloresActuales){
+                    if(dentro(flower.getPosX(),flower.getPosY(),this.puntoInicioX,this.puntoInicioY,
+                    masX,masY,maxX,maxY,menosX,menosY) && (flower.getColor()==this.getColorFav()||posibilidad())){
+                       
+                        /* SABER SI ENTRÓ UNA FLOR QUE NO ES DEL MISMO COLOR
+                        if(flower.getColor()!=this.getColorFav()){System.out.println("NO ES DEL MISMO COLOR");}//*/
+                        
+                        if(posibles.isEmpty()){posibles.add(Main.FloresActuales.indexOf(flower)); continue;}
+                        for (int i = 0; i < posibles.size(); i++) {
+                            a=posibles.get(i);
+                            if(flower.getPosX()<Main.FloresActuales.get(a).getPosX()){
+                                posibles.add(posibles.indexOf(a), Main.FloresActuales.indexOf(flower));
+                            }else posibles.add(posibles.indexOf(a)+1, Main.FloresActuales.indexOf(flower));
+                            break;}       
+                    }}
+            }            
+        }else{
+            int x,y;
+            for (int i = 0; i < 10; i++) {
+                x=(int)(Math.random() * Main.FloresActuales.size());
+                if(dentro(Main.FloresActuales.get(x).getPosX(),Main.FloresActuales.get(x).getPosY(),
+                this.puntoInicioX,this.puntoInicioY,masX,masY,maxX,maxY,menosX,menosY)){posibles.add(x);} 
+            }
+        }
+        /* PRINTS PARA PRUEBA
+        System.out.println("TERMINÓ BUSQUEDA");
+        System.out.println(posibles.size());//*/
+        recorrido(posibles);
     }
 
     @Override
